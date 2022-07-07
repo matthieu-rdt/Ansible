29a9e97a7e109bbe6adac64770564109fe2d5d6c
Matthieu <matt.doff@gmail.com>
2022-07-07 12:39:42 +0200

    Fixed bugs

--- installation_ansible.sh
+++ installation_ansible.sh
@@ -1,6 +1,6 @@
 echo "update packages"
-sudo apt-get update ; sudo upgrade
+sudo apt-get update ; sudo apt-get upgrade
 
 echo "Installing pip then ansible"
-sudo apt-get install python-pip
+sudo apt-get install python3-pip
 pip install ansible
